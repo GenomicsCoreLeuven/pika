b6824c35d4f9f78fbeeb035684c6a4869dbdaa3e
KoenHerten <koen.herten@kuleuven.be>
2016-10-06 08:44:08 +0200

    removed hardcoded vsc references

--- scripts/denovo/sga_preqc.epilog.sh
+++ scripts/denovo/sga_preqc.epilog.sh
@@ -3,8 +3,9 @@ module load SGA
 
 PROJECT_DIR="";
 OUTPUT_DIR="$PROJECT_DIR/sga_preqc";
+GENOME_DIR="";
 
 sga-preqc-report.py $OUTPUT_DIR/*.preqc;
 
 #compare to the sga standard examples
-sga-preqc-report.py -o standard_examples $OUTPUT_DIR/*.preqc /staging/leuven/stg_00019/genome/sga_preqc_examples/*.preqc;
+sga-preqc-report.py -o standard_examples $OUTPUT_DIR/*.preqc $GENOME_DIR/../../sga_preqc_examples/*.preqc;
